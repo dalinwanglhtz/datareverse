466ec80a02f952fd5635d51ab2889c50b2a55f1b
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-02 11:34:12 +1100

    first working cut

---
+++ src/com/codewardev/Kata.java
@@ -0,0 +1,42 @@
+package com.codewardev;
+
+// https://www.codewars.com/kata/569d488d61b812a0f7000015/train/java
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Kata {
+
+	public static int[] DataReverse(int[] data) {
+		
+		printData(data);
+		
+		int[][] newData = new int[data.length/8][8];
+		for(int i=0; i<newData.length; i++) {
+			newData[i] = Arrays.copyOfRange(data, i*8, i*8+8);
+		}
+		List<int[]> list = Arrays.asList(newData);
+		Collections.reverse(list);
+		int count=0;
+		int[] result = new int[data.length];
+		for(int i=0; i<newData.length; i++) {
+			for(int j=0; j<newData[i].length; j++) {
+				result[count++] = list.get(i)[j];
+			}
+		}
+		
+		printData(result);
+		
+		return result;
+	}
+
+	private static void printData(int[] data) {
+		System.out.println("This test: ");
+		for(int i=0; i<data.length; i++) {
+			System.out.print(data[i]+" ");
+		}
+		System.out.println();
+	}
+
+}
