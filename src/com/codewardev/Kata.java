8d2537fb39ddb2f811ba55d8595e713a7e35b66a
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-02 21:24:45 +1100

    refined working cut by refining algorithm

--- src/com/codewardev/Kata.java
+++ src/com/codewardev/Kata.java
@@ -3,22 +3,19 @@ package com.codewardev;
 // https://www.codewars.com/kata/569d488d61b812a0f7000015/train/java
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Kata {
 
+	private static final int BYTELENGTH = 8;
+	
 	public static int[] DataReverse(int[] data) {
-		int[][] newData = new int[data.length/8][8];
+		int[][] newData = new int[data.length/BYTELENGTH][BYTELENGTH];
 		for(int i=0; i<newData.length; i++) {
-			newData[i] = Arrays.copyOfRange(data, i*8, i*8+8);
+			newData[i] = Arrays.copyOfRange(data, i*BYTELENGTH, (i+1)*BYTELENGTH);
 		}
-		List<int[]> list = Arrays.asList(newData);
-		Collections.reverse(list);
 		int[] result = new int[data.length];
-		
-		for(int i=0; i<list.size(); i++) {
-			System.arraycopy(list.get(i), 0, result, i*8, 8);
+		for(int i=newData.length-1; i>=0; i--) {
+			System.arraycopy(newData[i], 0, result, (newData.length-i-1)*BYTELENGTH, BYTELENGTH);
 		}
 		
 		return result;
